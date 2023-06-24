223caaf28203c66e660c4a8b483ba16f057bfaae
Mauricio Tocura <mryutoc@gmail.com>
2023-06-24 12:48:26 -0300

    [initial-scaffolding] create initial scaffolding for application

---
+++ src/main/kotlin/com/tocura/study/kotlin/Application.kt
@@ -0,0 +1,11 @@
+package com.tocura.study.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
